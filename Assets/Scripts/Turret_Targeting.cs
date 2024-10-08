298f5dda4a826a4fcf9fa76229faa768b3938e20
Sean Butler <seanxbutler@gmail>
2024-10-08 00:10:37 -0700

    clean up Turret Target script

--- Assets/Scripts/Turret_Targeting.cs
+++ Assets/Scripts/Turret_Targeting.cs
@@ -19,21 +19,27 @@ using UnityEngine;
 
 public class Turret_Targeting : MonoBehaviour
 {
-    [SerializeField] private TurretBullet turret_bullet;
     public GameObject target_position;
-    public float detection_distance = 5.0f;
-    public float bullet_speed = 1.0f;
+    public float detection_distance = 10.0f;
+    public float bullet_distance_to_shoot = 10.0f;
+    public float bullet_speed = 4.0f;
+    public int number_of_bullets = 10;
+    public float turret_angle_movement = 45.0f;
 
+    [SerializeField] private TurretBullet turret_bullet;
+    [SerializeField] private List<int> ignoreAngles;
     private GameObject shootLocation;
     private List<TurretBullet> bullets = new List<TurretBullet>();
+    private int last_fixed_rot = 0;
     private TurretBullet lastBullet = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        shootLocation = this.transform.GetChild(4).gameObject;
+        const int target_location_child_index = 4; // relies on the target_location gameobject being the 4th child at the moment
+        shootLocation = this.transform.GetChild(target_location_child_index).gameObject;
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < number_of_bullets; i++)
         {
             bullets.Add(Instantiate(turret_bullet));
         }
@@ -50,39 +56,60 @@ public class Turret_Targeting : MonoBehaviour
         float turret_rotation_rad = Mathf.Atan2(direction.y, direction.x);
         float turret_rotation_deg = Mathf.Rad2Deg * turret_rotation_rad;
 
-        int fixed_positions = 45;
-        int fixed_rot = (int)((turret_rotation_deg / fixed_positions) + 0.5);
+        int fixed_positions = ((int)turret_angle_movement == 0) ? 1 : ((int)turret_angle_movement);   
 
-        turret_rotation_deg = Mathf.Abs((float)fixed_rot * 45.0f);
+        int fixed_rot = (int)((turret_rotation_deg / (float)fixed_positions) + 0.5f);
+        bool allow_rotation = CheckAnglesToIgnore(ref fixed_rot);
 
-        if (direction.y < 0)
-        {
-            turret_rotation_deg *= -1.0f;
-        }
+        turret_rotation_deg = Mathf.Abs((float)fixed_rot * turret_angle_movement);
+        turret_rotation_deg = (direction.y < 0) ? turret_rotation_deg *= -1.0f : turret_rotation_deg;
 
-        // TODO: bullets are shooting but sometimes up with the math
         if (distance < detection_distance)
         {
-            bool shoot_target = true;
             Vector3 bullet_direction = shootLocation.transform.position - this_obj_position.transform.position;
-            float bullet_distance = bullet_direction.magnitude;
-
-            if (lastBullet != null)
-            {
-                Vector3 last_bullet_direction = this_obj_position.transform.position - lastBullet.transform.position;
-                float last_bullet_distance = last_bullet_direction.magnitude;
-
-                if (Mathf.Abs(last_bullet_distance) < 5.0f)
-                {
-                    shoot_target = false;
-                }
-            }
-
+            bool shoot_target = CheckShootTarget();
             ShootTarget(bullet_direction, shoot_target);
-
         }
 
+        if (allow_rotation)
+        {
+            last_fixed_rot = fixed_rot;
+        }
+        
         this_obj_position.transform.eulerAngles = new Vector3(0.0f, 0.0f, turret_rotation_deg);
+    }
+
+    private bool CheckAnglesToIgnore(ref int fixed_rot)
+    {
+        bool allow_rotation = true;
+        foreach (var angle in ignoreAngles)
+        {
+            if (angle == fixed_rot)
+            {
+                allow_rotation = false;
+                fixed_rot = last_fixed_rot;
+                break;
+            }
+        }
+
+        return allow_rotation;
+    }
+
+    private bool CheckShootTarget()
+    {
+        bool shoot_target = true;
+        if (lastBullet != null)
+        {
+            Vector3 last_bullet_direction = this.transform.position - lastBullet.transform.position;
+            float last_bullet_distance = last_bullet_direction.magnitude;
+
+            if (Mathf.Abs(last_bullet_distance) < bullet_distance_to_shoot)
+            {
+                shoot_target = false;
+            }
+        }
+
+        return shoot_target;
     }
 
     private void ShootTarget(Vector3 bullet_direction, bool shoot_target)
